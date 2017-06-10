f50ea4379e763fa9c660a9e7cf2b61c7257b79b5
DVios-2 <ravidu0915@gmail.com>
2017-06-10 17:52:28 +0530

    Add changes to ldap login

--- routes/users.js
+++ routes/users.js
@@ -3,17 +3,17 @@ import express from 'express';
 var router = express.Router();
 var ldap = require('ldapjs');
 
-// /* GET users listing. */
-// router.get('/', function(req, res, next) {
-//   res.send('r
+const sambaserver = '1234';
+const user = 'dvios-2';
 
 const ldapClient = ldap.createClient({
     url: 'ldap://127.0.0.1:1389'
 });
 
 router.post('/sign-in', function(req, res, next) {
-   // res.writeHead(200, {'Content-Type': 'application/json'});
+   res.set('Content-Type', 'application/json');
 
+    let result ={};
     let username = req.body.username ;
     let password = req.body.password ;
 
@@ -21,29 +21,20 @@ router.post('/sign-in', function(req, res, next) {
     ldapClient.bind(`cn=${username}`, `${password}`, function (err) {
         if (err) {
             console.log('Binding error ', err);
+            result['success']= false;
         }else{
             console.log('success');
+            result['sharename']='shared1';
+            result['password']=sambaserver;
+            result['user']= user;
+            result['success']= true;
         }
+        res.send(JSON.stringify(result));
     });
 
 
-    // ###########################
 
-    let result = {"user": "dulaj", "password": "1234"};
-    //
-    // if(req.body.username === 'dulaj' && req.body.password === "1"){
-    //     result["sharename"] = "dulaj";
-    //     result["success"] = true;
-    // } else if(req.body.username === 'anuradha' && req.body.password === "2"){
-    //     result["sharename"] = "anuradha";
-    //     result["success"] = true;
-    // } else {
-    //     result = {"success":false};
-    // }
-    //
 
-    res.send(JSON.stringify(result));
-    // next();
 });
 
 module.exports = router;
