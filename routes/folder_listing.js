c8aa10be7221f1df92d9f5426f50cfec28ffd544
Anuradha Wickramarachchi <anuradhawick@gmail.com>
2017-06-10 13:17:07 +0530

    added env configuration

--- routes/folder_listing.js
+++ routes/folder_listing.js
@@ -1,12 +1,11 @@
-const conf = require('../conf');
 const express = require('express');
 const router = express.Router();
 const utils = require('../utils/utils');
-
+console.log(process.env.ROOT);
 router.get('/', function (req, res, next) {
     res.writeHead(200, {'Content-Type': 'application/json'});
 
-    let folderpath = conf.rootfolder + '/' + req.query.username;
+    let folderpath = process.env.ROOT+ '/' + req.query.username;
 
     let dirs = utils.allFolders(folderpath);
     res.end(JSON.stringify(dirs));
