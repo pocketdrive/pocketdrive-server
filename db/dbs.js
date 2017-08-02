ef6cabb186ca540b579fa72800b148ed8bbc1993
Dulaj Atapattu <dulaj.r.atapattu@gmail.com>
2017-08-02 22:23:17 +0530

    Merge branch 'master' of https://github.com/pocketdrive/pocketdrive-server
    Conflicts:
    	db/dbs.js

--- db/dbs.js
+++ db/dbs.js
@@ -2,5 +2,4 @@ import DataStore from 'nedb';
 
 export const userDb = new DataStore({filename: process.env.NE_DB_PATH_USER, autoload: true});
 export const accessDb = new DataStore({filename: process.env.NE_DB_PATH_ACCESS, autoload: true});
-export const accessDb = new DataStore({filename: process.env.NE_DB_PATH_ACCESS, autoload: true});
 export const linkShareDb = new DataStore({filename: process.env.NE_DB_PATH_LINK_SHARED_FILES, autoload: true});
