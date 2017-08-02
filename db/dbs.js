a599ae59085fae911fc0cdfe7919bd66cee40c6b
Dulaj Atapattu <dulaj.r.atapattu@gmail.com>
2017-08-02 21:54:35 +0530

    bug fixed

--- db/dbs.js
+++ db/dbs.js
@@ -2,3 +2,7 @@ import DataStore from 'nedb';
 
 export const userDb = new DataStore({filename: process.env.NE_DB_PATH_USER, autoload: true});
 export const accessDb = new DataStore({filename: process.env.NE_DB_PATH_ACCESS, autoload: true});
+
+const x = process.env.NE_DB_PATH_SYNC;
+
+console.log(x);
