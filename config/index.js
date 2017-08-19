39a289d342bfbf75d7285e4e7a34e6cb3cd38ce7
Anuradha Wickramarachchi <anuradhawick@gmail.com>
2017-08-19 12:23:12 +0530

    merging

--- config/index.js
+++ config/index.js
@@ -2,5 +2,5 @@
  * Created by anuradhawick on 7/31/17.
  */
 // TODO read these from the ENVIRONMENT
-export const centralServer = '10.10.10.69';
+export const centralServer = '192.248.8.242';
 export const centralServerWSPort = '8080';
