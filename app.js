ba2ab410e21d50115d91efbf6294f23edcc449df
Dulaj Atapattu <dulaj.r.atapattu@gmail.com>
2017-08-19 08:14:57 +0530

    app.js updated

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var users = require('./routes/users');
-var folder_listing = require('./routes/folder_listing');
-var share_folder = require('./routes/share_folder');
-var sync = require('./routes/sync');
-var ssdp = require('./utils/ssdp');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
-var app = express();
+const users = require('./routes/users');
+const sync = require('./routes/sync');
+const share = require('./routes/share');
+
+const ssdp = require('./utils/ssdp');
+
+const app = express();
 
 // import {Communicator} from './communicator/Communicator';
 
@@ -56,9 +57,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', users);
-app.use('/folder-list', folder_listing);
-app.use('/share-folder', share_folder);
 app.use('/sync', sync);
+app.use('/share', share);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
