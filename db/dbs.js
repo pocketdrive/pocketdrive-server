a8214ecabbfd3c3a4893f7f4a3daec2f418004fc
Dulaj Atapattu <dulaj.r.atapattu@gmail.com>
2017-10-26 14:54:12 +0530

    db updated

--- db/dbs.js
+++ db/dbs.js
@@ -1,7 +1,6 @@
 import DataStore from 'nedb';
 
 export const userDb = new DataStore({filename: process.env.NE_DB_PATH_USER, autoload: true});
-export const accessDb = new DataStore({filename: process.env.NE_DB_PATH_ACCESS, autoload: true});
 export const linkShareDb = new DataStore({filename: process.env.NE_DB_PATH_LINK_SHARED_FILES, autoload: true});
 export const syncMetadataDb = new DataStore({filename: process.env.NE_DB_PATH_SYNC_METADATA, autoload: true});
 export const syncDb = new DataStore({filename: process.env.NE_DB_PATH_SYNC, autoload: true});
